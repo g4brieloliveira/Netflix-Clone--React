3b599d881930b5a3e54eb81f7f7c4a950d5ef7e0
Gabriel Oliveira <g4bri3l.0lv@gmail.com>
2020-11-24 02:03:41 -0300

    Initial commit

---
+++ src/App.js
@@ -0,0 +1,78 @@
+import React, { useEffect, useState } from 'react';
+import Tmdb from './tmdb';
+
+import MovieRow from './components/MovieRow/MovieRow';
+import FeaturedMovie from './components/FeaturedMovie/FeaturedMovie';
+import Header from './components/Header/Header';
+
+import './App.css'
+
+export default () => {
+
+  const [movieList, setMovieList] = useState([]);
+  const [featuredData, setFeaturedData] = useState(null);
+  const [blackHeader, setblackHeader] = useState(false);
+ 
+  useEffect(()=>{
+    const loadAll = async () => {
+      let list = await Tmdb.getHomeList();
+      setMovieList(list);
+
+      let originals = list.filter(i => i.slug === 'originals');
+      let randomChosen = Math.floor(Math.random() * (originals[0].items.results.length -1));
+      let chosen = originals[0].items.results[randomChosen];
+
+      let chosenInfo = await Tmdb.getMovieInfo(chosen.id, 'tv');
+
+      setFeaturedData(chosenInfo);
+    }
+
+    loadAll();
+  }, []);
+
+  useEffect(() => {
+    const scrollListener = () => {
+      if(window.scrollY > 10) {
+        setblackHeader(true)
+      } else {
+        setblackHeader(false)
+      }
+    }
+
+    window.addEventListener('scroll', scrollListener)
+
+    return () => {
+      window.removeEventListener('scroll', scrollListener)
+    }
+  }, [])
+
+  return (
+    <div className="page">
+
+      <Header black={ blackHeader }/>
+
+      { featuredData && 
+        <FeaturedMovie item={ featuredData } />
+      } 
+
+      <section className="lists">
+        { movieList.map((item, key) => (
+          <MovieRow 
+            key={ key }
+            title={ item.title }
+            items={ item.items } />
+        ))}
+      </section>
+
+      <footer>
+        Feito com <span role="img" aria-label="coração">❤️</span> por <a href="https://github.com/g4brieloliveira">Gabriel Oliveira</a>
+      </footer>
+
+      { movieList.length <= 0 &&
+        <div className="loading">
+          <img src="https://www.filmelier.com/pt/br/news/wp-content/uploads/2020/03/netflix-loading.gif" alt="Carregando"/>
+        </div>
+      }
+    </div>
+  );
+}
